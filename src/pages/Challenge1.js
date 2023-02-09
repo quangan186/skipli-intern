0773879f8e82c41abc4f038b65f288ef1f468864
diablo <quangan186@gmail.com>
2023-02-10 00:06:05 +0700

    fetch api searchUser + getCode

--- src/pages/Challenge1.js
+++ src/pages/Challenge1.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Challenge1 = () => {
+  const [phoneNumber, setPhoneNumber] = useState()
+  const [otp, setOtp] = useState()
+
+  const onGetCodeClick = async(e) => {
+    e.preventDefault()
+    const phoneNumberRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
+    if (phoneNumberRegex.test(phoneNumber)){
+      const response = await fetch(`http://localhost:8080/api/auth/getCode`, phoneNumber)
+      const data = await response.json()
+      console.log(data)
+    } else{
+      console.log("Invalid!")
+    }
+  }
+
   return (
     <div>
       <div className="w-full flex flex-col gap-4">
@@ -9,11 +24,12 @@ const Challenge1 = () => {
             className="w-3/5 border px-4 py-2"
             type="text"
             placeholder="Please type your phone number with area code (E.g: +8412345689)"
-            onChange={(e) => console.log(e.target.value)}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            required
           />
           <button
             className="bg-blue-500 text-white w-[200px] h-[60px]"
-            type="submit"
+            onClick={onGetCodeClick}
           >
             Get verify code
           </button>
--- src/pages/Challenge2.js
+++ src/pages/Challenge2.js
@@ -1,24 +1,72 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import search from "../assets/search.svg";
 import heart from "../assets/heart.svg";
+import next from "../assets/next.svg";
+import prev from "../assets/previous.svg";
 import activeHeart from "../assets/activeHeart.svg";
 
 const Challenge2 = () => {
-  const [query, setQuery] = useState();
-  const [pageNumber, setPageNumber] = useState(1)
-    const [resultsPerPage, setResultsPerPage] = useState(100)
-    const [isLiked, setIsLiked] = useState(false);
+  const [query, setQuery] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [resultsPerPage, setResultsPerPage] = useState(100);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchData, setSearchData] = useState({
+    query: "",
+    pageNumber: 0,
+    resultsPerPage: 0,
+  });
 
-    const handleSearchUser = () => {
+  const [users, setUsers] = useState([]);
+  const [isLiked, setIsLiked] = useState(false);
 
-    }
-    
-    const handleLikedUser = () =>{
-        setIsLiked(state => !state)
-    }
+  const handleSearchUser = (e) => {
+    e.preventDefault();
+    setSearchData({
+      query: query,
+      pageNumber: pageNumber,
+      resultsPerPage: resultsPerPage,
+    });
+  };
+
+  useEffect(() => {
+    const getUsers = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        `http://localhost:8080/api/github/users/search?q=${searchData.query}&page=${searchData.pageNumber}&per_page=${searchData.resultsPerPage}`
+      );
+      const data = await response.json();
+      setUsers(data.users);
+      console.log(data.users);
+      setIsLoading(false);
+    };
+
+    getUsers();
+  }, [searchData]);
+
+  const onNextPageClick = () => {
+    setPageNumber(pageNumber + 1);
+    setSearchData({
+      query: query,
+      pageNumber: pageNumber + 1,
+      resultsPerPage: resultsPerPage,
+    });
+  };
+
+  const onPrevPageClick = () => {
+    setPageNumber(pageNumber - 1);
+    setSearchData({
+      query: query,
+      pageNumber: pageNumber - 1,
+      resultsPerPage: resultsPerPage,
+    });
+  };
+
+  const handleLikedUser = () => {
+    setIsLiked((state) => !state);
+  };
   return (
     <div>
-      <div className="py-8 px-12">
+      <form onSubmit={handleSearchUser} className="py-8 px-12">
         <div className="relative border">
           <input
             className="h-[60px] w-full px-4 pr-20"
@@ -28,17 +76,12 @@ const Challenge2 = () => {
               setQuery(e.target.value);
             }}
           />
-          <button 
+          <button
             type="submit"
             className="absolute right-0 top-1/2 w-[40px] h-[40px] -translate-x-1/2 -translate-y-1/2"
-            >
-            <img
-                src={search}
-                alt=""
-                className=""
-            />
+          >
+            <img src={search} alt="" className="" />
           </button>
-          
         </div>
 
         <div className="my-8">
@@ -49,40 +92,68 @@ const Challenge2 = () => {
               min="0"
               type="number"
               placeholder="Results per page"
-              onChange={e => setResultsPerPage(e.target.value)}
+              onChange={(e) => setResultsPerPage(e.target.value)}
             />
           </div>
+          {isLoading ? (
+            ""
+          ) : (
+            <>
+              <table className="my-4">
+                <tr>
+                  <th>Id</th>
+                  <th>Login</th>
+                  <th>Avatar URL</th>
+                  <th>HTML URL</th>
+                  <th>Public Repos</th>
+                  <th>Followers</th>
+                </tr>
+                {users && users.length > 0
+                  ? users.map((user, index) => {
+                      return (
+                        <tr key={index}>
+                          <td>{user.id}</td>
+                          <td>{user.login}</td>
+                          <td>{user.avatar_url}</td>
+                          <td>{user.html_url}</td>
+                          <td>{user.repos_url}</td>
+                          <td>{user.followers_url}</td>
+                          <td className="border-t border-t-black flex justify-center py-2">
+                            <button onClick={handleLikedUser}>
+                              <img
+                                src={isLiked ? activeHeart : heart}
+                                alt=""
+                                className="w-[40px] h-[40px]"
+                              />
+                            </button>
+                          </td>
+                        </tr>
+                      );
+                    })
+                  : ""}
+              </table>
 
-          <table className="my-4">
-            <tr>
-              <th>Id</th>
-              <th>Login</th>
-              <th>Avatar URL</th>
-              <th>HTML URL</th>
-              <th>Public Repos</th>
-              <th>Followers</th>
-            </tr>
-
-            <tr>
-              <td>aaa</td>
-              <td>aaa</td>
-              <td>aaa</td>
-              <td>aaa</td>
-              <td>aaa</td>
-              <td>aaa</td>
-              <td className="border-t border-t-black flex justify-center py-2">
-                <button onClick={handleLikedUser}>
-                    <img src={isLiked ? activeHeart : heart} alt="" className="w-[40px] h-[40px]" />
+              <div className="flex my-8 gap-4 justify-center items-center">
+                <button
+                  onClick={() => onPrevPageClick()}
+                  className={pageNumber <= 1 ? "invisible" : ""}
+                >
+                  <img src={prev} alt="" className="w-[40px] h-[40px]" />
                 </button>
-              </td>
-            </tr>
-          </table>
+                <button className="w-[40px] h-[40px] border">
+                  {pageNumber}
+                </button>
+                <button
+                  onClick={() => onNextPageClick()}
+                  className={(users && users.length > 0) || (users && users.length < resultsPerPage) ? "" : "invisible"}
+                >
+                  <img src={next} alt="" className="w-[40px] h-[40px]" />
+                </button>
+              </div>
+            </>
+          )}
         </div>
-
-        <div className="flex my-8">
-          <button className="w-[40px] h-[40px] border">1</button>
-        </div>
-      </div>
+      </form>
     </div>
   );
 };
